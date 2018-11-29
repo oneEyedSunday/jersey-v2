8934f8043c766b1dbcc3cbd52acdf4c063118b2b
oneEyedSunday <idiakosesunday@gmail.com>
2018-11-29 11:43:10 +0100

    Restructured architecture, and imports

---
+++ src/app/components/index.ts
@@ -0,0 +1,11 @@
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+
+export const components = [
+  AppComponent,
+  HomeComponent,
+];
+
+export * from './home/home.component';
+export * from './app.component';
+export default components;
