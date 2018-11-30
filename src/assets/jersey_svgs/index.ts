05d601d35ad3cf4e9d4cbc6ab0212122dcef7894
oneEyedSunday <idiakosesunday@gmail.com>
2018-11-30 17:06:24 +0100

    Added assets

---
+++ src/assets/jersey_svgs/index.ts
@@ -0,0 +1,47 @@
+export default [
+  {
+    'src': 'assets/jersey_svgs/152697.svg',
+    'title': 'Ajax',
+    'type': 'template'
+  },
+  {
+    'src': 'assets/jersey_svgs/152698.svg',
+    'title': 'Arsenal',
+    'type': 'template'
+  },
+  {
+    'src': 'assets/jersey_svgs/152699.svg',
+    'title': 'RetroMonaco',
+    'type': 'template'
+  },
+  {
+    'src': 'assets/jersey_svgs/152707.svg',
+    'title': 'RetroStripped',
+    'type': 'template'
+  },
+  {
+    'src': 'assets/jersey_svgs/152712.svg',
+    'title': 'RoundNeckPlainButton',
+    'type': 'template'
+  },
+  {
+    'src': 'assets/jersey_svgs/152713.svg',
+    'title': 'VNeckPlain',
+    'type': 'template'
+  },
+  {
+    'src': 'assets/jersey_svgs/152714.svg',
+    'title': 'PlainWithButton',
+    'type': 'template'
+  },
+  {
+    'src': 'assets/jersey_svgs/152717.svg',
+    'title': 'Plain',
+    'type': 'template'
+  },
+  {
+    'src': 'assets/jersey_svgs/152718.svg',
+    'title': 'Peru',
+    'type': 'template'
+  }
+];
