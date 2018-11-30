2989ded6955ad41c828e8e70e0eb8e62ad6424b6
oneEyedSunday <idiakosesunday@gmail.com>
2018-11-30 17:09:07 +0100

    Routing for Jersey Child routes

--- src/jersey/jersey-routing.module.ts
+++ src/jersey/jersey-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import * as fromContainers from './containers';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: fromContainers.CreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
