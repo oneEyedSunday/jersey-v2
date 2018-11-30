2a19c7e595c61776bbff07eeaa6278cefb98434a
oneEyedSunday <idiakosesunday@gmail.com>
2018-11-30 17:09:27 +0100

    Imported Angular Forms Module

--- src/jersey/jersey.module.ts
+++ src/jersey/jersey.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { JerseyRoutingModule } from './jersey-routing.module';
 import { reducers, metaReducers } from './store';
+import * as fromContainers from './containers';
+import * as fromComponents from './components';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     JerseyRoutingModule,
     StoreModule.forRoot(reducers, {metaReducers})
   ],
-  declarations: []
+  declarations: [
+    ...fromContainers.containers,
+    ...fromComponents.components,
+  ]
 })
 export class JerseyModule { }
