51349a2f68350433d15b86ceb916a0ddb10ef9f8
oneEyedSunday <idiakosesunday@gmail.com>
2018-11-30 19:11:40 +0100

    Template for Jersey extras

--- src/jersey/components/jersey-extras/jersey-extras.component.ts
+++ src/jersey/components/jersey-extras/jersey-extras.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { InitialJersey as DefaultJersey } from '../../models/jersey';
 @Component({
   selector: 'app-jersey-extras',
   templateUrl: './jersey-extras.component.html',
   styleUrls: ['./jersey-extras.component.scss']
 })
-export class JerseyExtrasComponent implements OnInit {
+export class JerseyExtrasComponent {
 
+  @Input() badges: any[];
+  @Output() selectBadgeEvent = new EventEmitter();
+  @Output() selectBadgePositionEvent = new EventEmitter();
+  badgeName: string;
+  badgePosition: string = DefaultJersey.badge_position;
+  public sides =  ['left', 'right', 'center'];
   constructor() { }
 
-  ngOnInit() {
+  selectBadge() {
+    this.selectBadgeEvent.emit(this.badgeName);
   }
 
+  selectBadgePosition() {
+    this.selectBadgePositionEvent.emit(this.badgePosition);
+  }
 }
