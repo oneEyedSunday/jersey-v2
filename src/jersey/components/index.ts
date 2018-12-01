1533aa9453e3f4cf26a04a08be2b92992ccce74a
oneEyedSunday <idiakosesunday@gmail.com>
2018-12-01 14:59:53 +0100

    Generated Jersey Preview component

--- src/jersey/components/index.ts
+++ src/jersey/components/index.ts
@@ -1,13 +1,16 @@
 import { JerseyStyleComponent } from './jersey-style/jersey-style.component';
 import { JerseyTextComponent } from './jersey-text/jersey-text.component';
 import { JerseyExtrasComponent } from './jersey-extras/jersey-extras.component';
+import { JerseyPreviewComponent } from './jersey-preview/jersey-preview.component';
 
 export const components: any[] = [
   JerseyExtrasComponent,
   JerseyStyleComponent,
   JerseyTextComponent,
+  JerseyPreviewComponent
 ];
 
 export * from './jersey-style/jersey-style.component';
 export * from './jersey-extras/jersey-extras.component';
 export * from './jersey-text/jersey-text.component';
+export * from './jersey-preview/jersey-preview.component';
---
+++ src/jersey/components/jersey-preview/jersey-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jersey-preview',
+  templateUrl: './jersey-preview.component.html',
+  styleUrls: ['./jersey-preview.component.scss']
+})
+export class JerseyPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
