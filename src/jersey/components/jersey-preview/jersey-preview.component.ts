972deb07d2b0bebbc48b9bfdbe5ecd68aff4b2f6
oneEyedSunday <idiakosesunday@gmail.com>
2018-12-01 15:52:09 +0100

    Created jersey Preview component

--- src/jersey/components/jersey-preview/jersey-preview.component.ts
+++ src/jersey/components/jersey-preview/jersey-preview.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy,
+  Input, OnChanges, SimpleChanges
+ } from '@angular/core';
+import { Jersey, InitialJersey as DefaultJersey } from '../../models/jersey';
 
 @Component({
   selector: 'app-jersey-preview',
   templateUrl: './jersey-preview.component.html',
-  styleUrls: ['./jersey-preview.component.scss']
+  styleUrls: ['./jersey-preview.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class JerseyPreviewComponent implements OnInit {
-
+export class JerseyPreviewComponent implements OnChanges {
+  @Input() jersey: Jersey;
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('changes', changes);
   }
-
 }
