fa8354979ae64cbd1fa3a99ac73ab992aa34170d
oneEyedSunday <idiakosesunday@gmail.com>
2018-12-02 08:25:56 +0100

    Jersey Preview: Dynamically inserting svg into component

--- src/jersey/components/jersey-preview/jersey-preview.component.ts
+++ src/jersey/components/jersey-preview/jersey-preview.component.ts
@@ -1,19 +1,39 @@
 import { Component, ChangeDetectionStrategy,
-  Input, OnChanges, SimpleChanges
+  Input, OnChanges, SimpleChanges,
+  ViewChild, ElementRef, OnInit,
  } from '@angular/core';
 import { Jersey, InitialJersey as DefaultJersey } from '../../models/jersey';
-
+import { SvgEditorService } from '../../core/svg-editor.service';
 @Component({
   selector: 'app-jersey-preview',
   templateUrl: './jersey-preview.component.html',
   styleUrls: ['./jersey-preview.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class JerseyPreviewComponent implements OnChanges {
+export class JerseyPreviewComponent implements OnInit, OnChanges {
   @Input() jersey: Jersey;
-  constructor() { }
+  @ViewChild('svgObject') svgObjectRef: ElementRef;
+  _svgEditor: SvgEditorService;
+  editorInitialized = false;
+  constructor(private svgEditor: SvgEditorService) {
+    this._svgEditor = svgEditor;
+  }
+
+  ngOnInit() {
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     console.log('changes', changes);
+    if (changes.jersey.previousValue === undefined) {
+      console.log('init');
+    } else {
+      if (this.editorInitialized) {
+      } else {
+        this._svgEditor.setReferenceToSvg(this.svgObjectRef.nativeElement.contentDocument);
+        this.editorInitialized = true;
+      }
+      // console.log(this.svgObjectRef.nativeElement.contentDocument.childNodes[0]);
+      this._svgEditor.repaintSvg();
+    }
   }
 }
