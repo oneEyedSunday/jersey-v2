84bed4e842c0e28b1dee6294e1878718e99d501e
oneEyedSunday <idiakosesunday@gmail.com>
2018-12-02 08:27:02 +0100

    SVG Editor: Service to manipulate svg

--- src/jersey/core/svg-editor.service.ts
+++ src/jersey/core/svg-editor.service.ts
@@ -1,9 +1,49 @@
+import { Observable, Subscription } from 'rxjs';
 import { Injectable } from '@angular/core';
+import Svgs from '../../assets/jersey_svgs';
+import { Store, select } from '@ngrx/store';
+import { getJersey } from '../store';
+import { Jersey } from '../models/jersey';
+import { ElementRef } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SvgEditorService {
+  jerseyState$: Observable<Jersey>;
+  jerseyStateSubscription: Subscription;
+  jersey: Jersey;
+  svg: any;
+  constructor(private store: Store<any>) {
+    this.jerseyState$ = store.pipe(select(getJersey));
+    this.jerseyStateSubscription = this.jerseyState$.subscribe(state => this.jersey = state);
+  }
 
-  constructor() { }
+  get sourceFromTemplateName(): string {
+    return Svgs.filter(svg => svg.title === this.jersey.base)[0].src || '';
+  }
+
+  setReferenceToSvg(svgRef: ElementRef) {
+    this.svg = svgRef;
+    this.svg = this.svg.childNodes[0];
+    // this.svg = this.svg.nativeElement.contentDocument.childNodes[0];
+    // console.log(this.svg);
+    // console.log(this.svg.contentDocument);
+    // console.log(this.svg.nativeElement);
+    // console.log(this.svg.nativeElement.contentDocument);
+    // console.log(this.svg.contentDocument.documentElement);
+    // console.log(this.svg.contentDocument.getElementsByTagName('svg'));
+  }
+
+  repaintSvg() {
+    if (this.jersey.text) { this.paintText('text'); }
+  }
+
+  paintText(type: string) {
+    // console.log(this.svg);
+    console.log(this.svg.children);
+    const children: Array<any> = Array.from(this.svg.children);
+    const toReplace = type === 'kit_number' ? 'kitn' : 'sponsor';
+    console.log(children);
+  }
 }
